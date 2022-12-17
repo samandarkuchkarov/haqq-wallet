0f51e482e827e8b69b5b1ad65d2e773226d4c161
samandar <samandar8939522@gmail.com>
2022-12-17 13:23:36 +0500

    init

--- ios/haqq/AppDelegate.h
+++ ios/haqq/AppDelegate.h
@@ -1,7 +1,11 @@
 #import <React/RCTBridgeDelegate.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
+
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, RNAppAuthAuthorizationFlowManager>
+
+@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 
 @property (nonatomic, strong) UIWindow *window;
 
